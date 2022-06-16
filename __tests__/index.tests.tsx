fc47e1d205c7b5cd76d0c553b2faf6f28dcba5a2
Vitor Sanches <vitor.sanches@enviabybus.com.br>
2022-06-16 16:17:41 -0300

    feat: move pages/ and styles/ to src/
    Summary:
    - Creates a src/ folder and move pages/ and styles/ folders inside it.
    
    Test cases:
    1. App should be building and running normally

--- __tests__/index.tests.tsx
+++ __tests__/index.tests.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-import Home from '../pages/index';
+import Home from '../src/pages/index';
 
 describe('Home', () => {
   it('renders a heading', () => {

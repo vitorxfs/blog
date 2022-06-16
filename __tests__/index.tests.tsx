f125e27e24ceb59bc009f4fe593fc82af0e239eb
Vitor Sanches <vitor.sanches@enviabybus.com.br>
2022-06-16 15:45:30 -0300

    feat: remove create-next-app code that won't be used

--- __tests__/index.tests.tsx
+++ __tests__/index.tests.tsx
@@ -10,7 +10,7 @@ describe('Home', () => {
     render(<Home />);
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i
+      name: /Vitor Sanches' Blog/
     });
 
     expect(heading).toBeInTheDocument();

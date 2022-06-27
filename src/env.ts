12c538ca55ea65c9587e5050a792505560a42ec5
Vitor Sanches <viisanches98@gmail.com>
2022-06-26 23:10:02 -0300

    feat: add prismic cms entrypoint to environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,7 @@
+// Public
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL;
 export const LINKEDIN_URL = process.env.NEXT_PUBLIC_LINKEDIN_URL;
 export const TWITTER_URL = process.env.NEXT_PUBLIC_TWITTER_URL;
+
+// Private
+export const PRISMIC_CMS_ENTRYPOINT = process.env.PRISMIC_CMS_ENTRYPOINT;

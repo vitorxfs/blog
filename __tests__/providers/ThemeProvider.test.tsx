744d6f1dec707f61134396df518bb13f635e6d6b
Vitor Sanches <vitor@LAPTOP-VHR72ONE.localdomain>
2022-08-25 18:40:16 -0300

    test: modify ThemeProvider tests to consider local storage theme definition

--- __tests__/providers/ThemeProvider.test.tsx
+++ __tests__/providers/ThemeProvider.test.tsx
@@ -5,19 +5,38 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { getLuminance } from 'polished';
 import { ThemeConsumer } from 'styled-components';
 
-import ThemeProvider, { ThemeSwitcherContext } from '../../src/common/providers/ThemeProvider';
+import { LOCAL_STORAGE_THEME_KEY } from '../../src/env';
+import ThemeProvider, { Theme, ThemeSwitcherContext } from '../../src/common/providers/ThemeProvider';
+
+const BackgroundColorComponent = () => {
+  return (
+    <ThemeProvider>
+      <ThemeConsumer>
+        {
+          (value) => <><h1>{value.colors.background}</h1></>
+        }
+      </ThemeConsumer>
+    </ThemeProvider>
+  )
+}
 
 describe('ThemeProvider', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('sets local storage theme when it exists', () => {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, Theme.Dark);
+
+    render(<BackgroundColorComponent />);
+
+    const backgroundColor = screen.getByRole('heading').textContent;
+
+    expect(backgroundColor && getLuminance(backgroundColor) < 0.5).toBeTruthy();
+  })
+
   it('sets light theme by default', () => {
-    render(
-      <ThemeProvider>
-        <ThemeConsumer>
-          {
-            (value) => <><h1>{value.colors.background}</h1></>
-          }
-        </ThemeConsumer>
-      </ThemeProvider>
-    );
+    render(<BackgroundColorComponent />);
 
     const backgroundColor = screen.getByRole('heading').textContent;
 
